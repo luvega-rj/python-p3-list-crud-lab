15d3132fd92f64e2a5fcd6acf4884da6586795fe
luvega-rj <luvegaronny@gmail.com>
2023-05-22 08:42:39 +0300

    done

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,29 @@
 def create_an_empty_list():
-    return None
-
+    return []
 def create_a_list():
-    return None
-
+    places =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
+    return places
 def add_element_to_end_of_list(l, element):
-    return None
-
+    l =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
+    l.append(element)
+    return l
 def add_element_to_start_of_list(l, element):
-    return None
-
+    l =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
+    l.insert(0, element)
+    return l
 def remove_element_from_end_of_list(l):
-    return None
-
+    l.pop(-1)
+    return l
+l =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
 def remove_element_from_start_of_list(l):
-    return None
-
+    del l[0]
+    return l
+l =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
 def retrieve_first_element_from_list(l):
-    return None
-
+    x = l[0]
+    return x
+l =  ["WestLands", "kisumu", "Kitusuru", "EastLands"]
 def retrieve_element_from_index(l, index):
-    return None
-
+    return l[index]
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
